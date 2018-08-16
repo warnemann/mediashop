8e8f1aebbc2042b39febee75a1c8b495dcbd2e15
warnemann <warnemann@gmail.com>
2018-08-16 15:09:07 +0200

    0.0.3

--- src/Providers/mediashopServiceProvider.php
+++ src/Providers/mediashopServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace mediashop\Providers;
 
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
+
 
 class mediashopServiceProvider extends ServiceProvider
 {
@@ -14,4 +18,19 @@ class mediashopServiceProvider extends ServiceProvider
   {
 
   }
+
+  /**
+ * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
+ */
+public function boot(Twig $twig, Dispatcher $eventDispatcher)
+  {
+      $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
+      {
+         $partial->set('footer', 'mediashop::content.mediashopFooter');
+      }, 0);
+      return false;
+  }
+
+
+
 }
