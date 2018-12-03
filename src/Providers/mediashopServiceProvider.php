641fc20c2dcb70269c7762ba9ae737ac1957abda
warnemann <warnemann@gmail.com>
2018-12-03 14:56:48 +0100

    1456

--- src/Providers/mediashopServiceProvider.php
+++ src/Providers/mediashopServiceProvider.php
@@ -56,12 +56,12 @@ class mediashopServiceProvider extends ServiceProvider
                                          $container->setNewComponentTemplate('mediashop::Item.Components.SingleItem');
                                          }
 
-/**                                  if ($container->getOriginComponentTemplate()=='Ceres::Item.Components.VariationSelect')
+                                  if ($container->getOriginComponentTemplate()=='Ceres::Item.Components.VariationSelect')
                                          {
                                          $container->setNewComponentTemplate('mediashop::Item.Components.VariationSelect');
                                          }
-*/
-                                      if ($container->getOriginComponentTemplate()=='Ceres::Item.SingleItemWrapper')
+
+                                     if ($container->getOriginComponentTemplate()=='Ceres::Item.SingleItemWrapper')
                                          {
                                          $container->setNewComponentTemplate('mediashop::Item.mediashopSingleItemWrapper');
                                          }
